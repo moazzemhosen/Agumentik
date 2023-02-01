132670413e9b754aab4ac229a399924bb7ad76b0
MOAZZEM HOSEN SARDAR <92745004+moazzemhosen@users.noreply.github.com>
2023-02-01 14:50:50 +0530

    port

--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,7 @@ const app = require("./index");
 const connect = require("./configs/db");
  require("dotenv").config();
 //let PORT = process.env.PORT;
-app.listen(process.env.PORT, async function () {
+app.listen(8080, async function () {
   try {
     await connect();
     console.log(`server is running on port ${PORT}`);
