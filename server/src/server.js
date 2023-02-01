3666f4be89a75093ee52d2d67e548d914a6dbe4f
MOAZZEM HOSEN SARDAR <92745004+moazzemhosen@users.noreply.github.com>
2023-02-01 13:42:35 +0530

    file

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,7 @@
 const app = require("./index");
 const connect = require("./configs/db");
 // require("dotenv").config();
-let PORT = process.env.PORT || 5000;
+let PORT = process.env.PORT || 8080;
 app.listen(PORT, async function () {
   try {
     await connect();
